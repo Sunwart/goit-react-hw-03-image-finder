97645887e7f3352a3e42dfd8e56df1f79952d51a
Sunwart <li.sa.vectorart@gmail.com>
2022-01-25 00:39:42 +0200

    bugfixing

--- src/components/ImageGallery/ImageGallery.styled.js
+++ src/components/ImageGallery/ImageGallery.styled.js
@@ -7,7 +7,7 @@ export const ImageGalleryContainer = styled.ul`
   grid-gap: 16px;
   margin-top: 0;
   margin-bottom: 0;
-  padding-top: 16px;
+  padding: 16px 0 0 0;
   list-style: none;
   margin-left: auto;
   margin-right: auto;
